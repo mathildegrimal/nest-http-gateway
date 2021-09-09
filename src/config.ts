16647b93679af379e1f907eabd6a79cd4d6dfdad
Mathilde Grimal <mat.grimal@gmail.com>
2021-09-09 12:43:12 +0200

    changes on config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const MESSAGE_BROKER_PORT = '';
-export const MESSAGE_BROKER_HOST = '';
-export const EVENT_STORE_HOST = '';
-export const EVENT_STORE_PORT = '';
+export const MESSAGE_BROKER_PORT = '4222';
+export const MESSAGE_BROKER_HOST = 'localhost';
+export const EVENT_STORE_HOST = 'localhost';
+export const EVENT_STORE_PORT = '2113';
